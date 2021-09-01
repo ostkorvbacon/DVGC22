27e23b21056198dcebb1f7d932bdc8be4ade0508
eriksven107 <57899290+eriksven107@users.noreply.github.com>
2021-09-01 17:41:44 +0200

    Add files via upload

---
+++ Ny mapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView a = findViewById(R.id.texttest);
+        a.setText("Hej igen!!!!!!");
+    }
+}
