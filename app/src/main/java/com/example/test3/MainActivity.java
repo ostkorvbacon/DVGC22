679f38bd9def50cde80070e9c3ea4c718a82cc6e
LeonardSchick <66326752+LeonardSchick@users.noreply.github.com>
2021-09-24 15:31:34 +0200

    register refinements + code cleaned

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -7,40 +7,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-<<<<<<< Updated upstream
-import com.example.test3.DataExtraction.CovidData;
-import com.example.test3.DataExtraction.DataExtractor;
-
-
-public class MainActivity extends AppCompatActivity {
-    private CovidData covidData;
-    public boolean loginAttempt(String loginId, String password){
-
-        return loginId.equals("login");
-        //pseudokod
-        //if loginId in user database
-        //if user.password.matches(password)
-        //return true
-        //else
-        //return false
-=======
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
+import com.example.test3.DataExtraction.DataExtractor;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 
 public class MainActivity extends AppCompatActivity {
 
     String res = null;
-
 
     public boolean loginAttempt(String loginId, String password){
 
@@ -77,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         );
         queue.add(loginReq);
         return false;
->>>>>>> Stashed changes
     }
 
     @Override
@@ -89,13 +67,8 @@ public class MainActivity extends AppCompatActivity {
         downloadCovidDataThread.start();
 
         Button loginButton = findViewById(R.id.login_button);
-<<<<<<< Updated upstream
-        ProgressBar loadingProgressBar = findViewById(R.id.loading);
-
-=======
         Button createButton = findViewById(R.id.create_button);
         ProgressBar loadingProgressBar = findViewById(R.id.loading);
-
 
         //register listener
         createButton.setOnClickListener(new View.OnClickListener() {
@@ -106,38 +79,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         //login listener
->>>>>>> Stashed changes
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 loadingProgressBar.setVisibility(view.VISIBLE);
-<<<<<<< Updated upstream
-
-                String loginId = ((EditText)findViewById(R.id.loginId)).getText().toString();
-                String password = ((EditText)findViewById(R.id.password)).getText().toString();
-
-                boolean success = loginAttempt(loginId, password);
-
-                if(success){
-                    //check if user is an admin and direct accordingly to the respective pages
-                    Intent loginIntent = new Intent(getApplicationContext(), MainMenuActivity.class);
-                    loginIntent.putExtra("loginId", loginId);
-                    startActivity(loginIntent);
-                }else{
-                    loadingProgressBar.setVisibility(view.GONE);
-                    Toast toast = Toast.makeText(getApplicationContext(),"Wrong username or password",Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-
-
-=======
                 String loginId = ((EditText)findViewById(R.id.loginId)).getText().toString();
                 String password = ((EditText)findViewById(R.id.password)).getText().toString();
                 loginAttempt(loginId, password);
                 loadingProgressBar.setVisibility(view.GONE);
->>>>>>> Stashed changes
             }
         });
     }
