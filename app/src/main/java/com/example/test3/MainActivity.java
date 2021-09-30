6be1840f910ceee6b8e8246c56de2db3b6401721
dooote <wowdooote@live.se>
2021-09-30 13:57:33 +0200

    Cleaned up the code

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        handler.testAPIFunctions();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
