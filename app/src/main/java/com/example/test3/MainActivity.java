7799521847f63cc147e6b8934388ebe2740c1e63
dooote <wowdooote@live.se>
2021-10-27 15:07:17 +0200

    fixed an issue that caused a fully vaccinated user to get the pop up for second dose

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
 /*
         if(handler.testAPIFunctions()){
             Log.i("API test", "Success!");
